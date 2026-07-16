01cf6b56621a627490d8305c02fc28f98cfa9df2
kjmalswo-game <kimalswo3922@gmail.com>
2026-07-16 20:37:11 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -739,13 +739,13 @@ function startMatchPhase(roomCode, isSecondHalf = false) {
                         let pinchedY = 50 + (p.baseY - 50) * 0.35 + organicY;
                         if (p.team === state.possessionTeam) {
                             // 캐칭한 팀 (공격 전개 준비) - 보통 역습을 위해 골킥보다 라인을 높게 올립니다.
-                            if (p.role === 'FW') { targetX = 50 + (dir * 3) + organicX; targetY = p.baseY; } // 하프라인 넘어서 대기
-                            else if (p.role === 'MF') { targetX = 50 - (dir * 20) + organicX; targetY = p.baseY; } 
-                            else { targetX = 50 - (dir * 36) + organicX; targetY = p.baseY; } 
+                            if (p.role === 'FW') { targetX = 50 - (dir * 6) + organicX; targetY = p.baseY; } // 하프라인 넘어서 대기
+                            else if (p.role === 'MF') { targetX = 50 - (dir * 25) + organicX; targetY = p.baseY; } 
+                            else { targetX = 50 - (dir * 38) + organicX; targetY = p.baseY; } 
                         } else {
                             // 수비하는 팀 (빠르게 복귀)
-                            if (p.role === 'FW') { targetX = 50 + (dir * 26) + organicX; targetY = p.baseY; }
-                            else if (p.role === 'MF') { targetX = 50 + (dir * 16) + organicX; targetY = p.baseY; }
+                            if (p.role === 'FW') { targetX = 50 + (dir * 7) + organicX; targetY = p.baseY; }
+                            else if (p.role === 'MF') { targetX = 50 + (dir * 3) + organicX; targetY = p.baseY; }
                             else if (p.role === 'DF') { targetX = 50 - (dir * 7) + organicX; targetY = p.baseY; }
                         }
                     }
